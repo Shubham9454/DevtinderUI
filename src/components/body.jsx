534b86d56ee5dff1334f51774261488be4b059bf
Shubham9454 <shubhamy.9454@gmail.com>
2025-08-03 22:20:02 +0530

    Redirect to login if not authenticated

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -4,14 +4,16 @@ import Footer from "./Footer";
 import { useEffect } from "react";
 import axios from "axios";
 import BASE_URL from "../utils/constants";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../utils/userSlice";
 
 const Body = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const userFromStore = useSelector((store) => store.user);
 
     const fetchUser = async () =>{
+        if(userFromStore) return;
         try{
             const res = await axios.get(BASE_URL + "/profile/view" , {
                 withCredentials: true
