2a030c4743941c2c50cbf2242266a0eed76741ac
Shubham9454 <shubhamy.9454@gmail.com>
2025-08-29 14:51:17 +0530

    Activate the like and dislike button of feed page

--- src/components/userCard.jsx
+++ src/components/userCard.jsx
@@ -1,5 +1,35 @@
+import axios from "axios";
+import BASE_URL from "../utils/constants";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
 const UserCard = ({ user }) => {
-  const { firstName, lastName, age, gender, about, photoURL, skills } = user;
+  const { _id , firstName, lastName, age, gender, about, photoURL, skills } = user;
+
+  const [success , setSuccess] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleConnection = async (status , userId) =>{
+    try{
+
+      const res = await axios.post(
+        BASE_URL + "/request/send/" + status + "/" + userId ,
+        {} , 
+        {withCredentials: true}
+      );
+      
+      dispatch(removeFeed(userId));
+      setSuccess(true);
+
+      setTimeout(() =>{
+        return setSuccess(false);
+      } , 3000);
+
+
+    } catch(err){
+      console.error(err.data);
+    }
+  }
 
   return (
     <div className="card bg-base-200 w-96 shadow-sm">
@@ -14,10 +44,13 @@ const UserCard = ({ user }) => {
 
         <p>{about}</p>
         <div className="card-actions justify-end my-2">
-          <button className="btn btn-primary">Like</button>
-          <button className="btn btn-soft">Dislike</button>
+          <button className="btn btn-primary" onClick={() => handleConnection("interested" , _id)}>Like</button>
+          <button className="btn btn-soft" onClick={() => handleConnection("ignored" , _id)}>Dislike</button>
         </div>
       </div>
+      {success && <div className="flex justify-end fixed up-5 right-1 z-50">
+          <Alert type="success" />
+      </div>} 
     </div>
   );
 };
