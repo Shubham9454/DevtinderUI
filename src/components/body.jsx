5177402f35a69299af0fec344d729c3b19efbb5d
Shubham9454 <shubhamy.9454@gmail.com>
2025-09-01 21:58:57 +0530

    some minor changes

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -8,37 +8,34 @@ import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../utils/userSlice";
 
 const Body = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const userFromStore = useSelector((store) => store.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const userFromStore = useSelector((store) => store.user);
 
-    const fetchUser = async () =>{
-        if(userFromStore) return;
-        try{
-            const res = await axios.get(BASE_URL + "/profile/view" , {
-                withCredentials: true
-            });
-            dispatch(addUser(res.data)); 
-        }
-        catch(err){
-            if(err.status === 401)
-            navigate("/login");
-            console.error(err);
-        };
+  const fetchUser = async () => {
+    if (userFromStore) return;
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view",
+        {withCredentials: true}
+      );
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) navigate("/login");
+      console.error(err);
     }
+  };
 
-    useEffect(() =>{
-        fetchUser();
-    }, []);
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
-    return (
-        <div>
-            <Navbar />
-            <Outlet />
-            <Footer />
-
-        </div>
-    );
-}
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 export default Body;
