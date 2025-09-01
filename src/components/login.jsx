3208b80f16a49d6358b7a109b6eb4cf44d3fd997
Shubham9454 <shubhamy.9454@gmail.com>
2025-09-01 21:59:27 +0530

    added the registration page in login componet

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -5,22 +5,22 @@ import { addUser } from "../utils/userSlice";
 import { useNavigate } from "react-router-dom";
 import BASE_URL from "../utils/constants";
 
-
-
 const Login = () => {
   const userFromStore = useSelector((store) => store.user);
   const navigate = useNavigate();
 
-  if(userFromStore) return navigate("/");
+  if (userFromStore) return navigate("/");
 
-  const [emailID, setEmail] = useState("john123@gmail.com");
-  const [password, setPassword] = useState("John@123");
-  const [error , setError] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [emailID, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoginForm, setIsLoginForm] = useState(true);
+  const [error, setError] = useState("");
 
   const dispatch = useDispatch();
-  
+
   const handleLogin = async () => {
-    
     try {
       const res = await axios.post(
         BASE_URL + "/login",
@@ -33,25 +33,62 @@ const Login = () => {
 
       //console.log("Login Successful: " + res.data);
       dispatch(addUser(res.data));
-      navigate("/")
-      
+      navigate("/");
     } catch (err) {
-      setError(err?.response?.data  || "Something Went Wrong");
+      setError(err?.response?.data || "Something Went Wrong");
       // console.error(err);
     }
   };
+
+  // function to call the SignUp API
+  const handleSignup = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        { firstName, lastName, emailID, password },
+        { withCredentials: true }
+      );
+      
+      navigate("/login");
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   return (
     <div className="flex justify-center my-20">
       <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-        <legend className="fieldset-legend">Log in to your account</legend>
+        <legend className="fieldset-legend">
+          {isLoginForm ? "Login to your Account" : "Sign Up as a new user"}
+        </legend>
         <div className="text-red-400">{error}</div>
 
-        <label className="label">Email</label>
+        {!isLoginForm && (
+          <>
+            <label className="label">First Name</label>
+            <input
+              type="firstName"
+              value={firstName}
+              className="input"
+              placeholder="First Name"
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <label className="label">Last Name</label>
+            <input
+              type="lastName"
+              value={lastName}
+              className="input"
+              placeholder="Last Name"
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </>
+        )}
+        <label className="label">Email Id</label>
         <input
           type="email"
           value={emailID}
           className="input"
-          placeholder="Email"
+          placeholder="Email Id"
           onChange={(e) => setEmail(e.target.value)}
         />
 
@@ -63,9 +100,18 @@ const Login = () => {
           placeholder="Password"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="btn btn-neutral mt-4" onClick={handleLogin}>
-          Login
+        <button
+          className="btn btn-neutral mt-4"
+          onClick={isLoginForm ? handleLogin : handleSignup}
+        >
+          {isLoginForm ? "Login" : "Sign Up"}
         </button>
+        <p
+          className="flex justify-center my-2 cursor-pointer"
+          onClick={() => setIsLoginForm((value) => !value)}
+        >
+          {isLoginForm ? "New User? Sign up Here" : "Existing user? Login Here"}
+        </p>
       </fieldset>
     </div>
   );
