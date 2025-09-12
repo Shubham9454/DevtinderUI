de7148bfedfa9347d290d4149fcc302cd0cdb935
Shubham9454 <shubhamy.9454@gmail.com>
2025-09-12 10:52:32 +0530

    emptied the vercel.json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter , Routes , Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./components/body";
 import Feed from "./components/Feed";
 import Login from "./components/login";
@@ -8,26 +8,23 @@ import appStore from "./utils/appStore";
 import Connections from "./components/Connections";
 import Requests from "./components/Request";
 import LandingPage from "./components/Landingpg";
- 
+
 function App() {
   return (
-    <>  
-    <Provider store={appStore}>
-    <BrowserRouter basename= "/">
-    <Routes>
-      
-      <Route path="/" element={<Body />}>
-      <Route path="/" element={<Feed />} /> 
-      <Route path="/login" element={<Login />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/connections" element={<Connections />} />
-      <Route path="/requests" element={<Requests />} />
-
-      </Route>
-    </Routes>
-    
-    </BrowserRouter>
-    </Provider>
+    <>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
